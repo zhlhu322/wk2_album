8087f2f5c9b0d0a6889a02008f28b0cc523abd8f
zhlhu322 <0322enter@gmail.com>
2024-03-05 11:42:40 +0800

    wk2_fin

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <Header />
+      <AlbumList />
+    </SafeAreaView>
+      
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:"#252423"
   },
 });
+
+export default App;
